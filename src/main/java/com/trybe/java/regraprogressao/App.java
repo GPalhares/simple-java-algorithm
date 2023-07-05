835b4e508abbc0b9ed089a7fb92074a1e0f42f2a
Gabriel <devpalhares@gmail.com>
2023-07-05 18:14:04 -0300

    feat: Cadastra as atividades

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,21 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int quantityActivities = scanner.nextInt();
+    scanner.nextLine();
+
+    String[] nameActivities = new String[quantityActivities];
+    int[] wightActivity = new int[quantityActivities];
+
+    for (int i = 0; i < quantityActivities; i++) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ":");
+      nameActivities[i] = scanner.nextLine();
+
+      System.out.println("Digite o peso da atividade " + (i + 1) + ":");
+      wightActivity[i] = scanner.nextInt();
+      scanner.nextLine(); // Limpar o buffer do scanner
+    }
   }
 }
