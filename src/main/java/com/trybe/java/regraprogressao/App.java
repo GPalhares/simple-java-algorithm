eb2daec3838fea876b9e8dcaa80221d5ed6df214
Gabriel <devpalhares@gmail.com>
2023-07-05 18:49:40 -0300

    feat: Emite o resultado correto

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -19,6 +19,8 @@ public class App {
     String[] nameActivities = new String[quantityActivities];
     int[] wightActivity = new int[quantityActivities];
     int[] notaActivity = new int[quantityActivities];
+    int notaFinal = 0;
+    int pesoFinal = 0;
 
     for (int i = 0; i < quantityActivities; i++) {
       System.out.println("Digite o nome da atividade " + (i + 1) + ":");
@@ -28,6 +30,20 @@ public class App {
       System.out.println("Digite a nota obtida para " + nameActivities[i] + ":");
       notaActivity[i] = scanner.nextInt();
       scanner.nextLine();
+      notaFinal += notaActivity[i] * wightActivity[i];
+      pesoFinal += wightActivity[i];
     }
+
+    if (notaFinal / pesoFinal >= 85) {
+      System.out.println("Parabéns! Você alcançou " + notaFinal / pesoFinal + "%!"
+              + " E temos o prazer de informar que você obteve aprovação! ");
+    }
+
+    if (notaFinal / pesoFinal < 85) {
+      System.out.println("Lamentamos informar que,"
+              + " com base na sua pontuação alcançada neste período, " + notaFinal / pesoFinal
+              + "%, você não atingiu a pontuação mínima necessária para sua aprovação. ");
+    }
+
   }
 }
